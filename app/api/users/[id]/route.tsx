da123e070adddc294d5bffe8769ec403e6b6df1f
Olivier <iradukundaolivier2000@gmail.com>
2025-01-03 17:02:56 +0200

    feat: I added way to updating an object

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,3 +1,4 @@
+import { error } from "console";
 import { NextRequest, NextResponse } from "next/server";
 
 export function GET(
@@ -8,4 +9,24 @@ export function GET(
     return NextResponse.json({ error: "User not found" }, { status: 404 });
 
   return NextResponse.json({ id: 1, name: "Mosh" });
+}
+
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  // validate the request body
+  // If invalid, return 400
+  // Fetch the user with the given id
+  // If doesn't exist, return 404
+  // Update the user
+  // Return the updated user
+  const body = await request.json();
+  if (!body.name)
+    return NextResponse.json({ error: "Name is required" }, { status: 400 });
+
+  if (params.id > 10)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+
+  return NextResponse.json({ id: 1, name: body.name });
 }
