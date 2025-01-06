15200f9ca1f0382422db30191468df5e42cae284
Olivier <iradukundaolivier2000@gmail.com>
2025-01-07 00:16:33 +0200

    feat: Configuring Gooogle Provider

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -3,12 +3,15 @@ import Link from "next/link";
 
 const NavBar = () => {
   return (
-    <div className="flex bg-slate-200 p-5">
+    <div className="flex bg-slate-200 p-5 space-x-3">
       <Link href="/" className="'mr-5">
         Next.js
       </Link>
       <Link href="/users" className="ml-5">
         Users
+      </Link>
+      <Link href="/api/auth/signin" className="ml-5">
+        Login
       </Link>
     </div>
   );
